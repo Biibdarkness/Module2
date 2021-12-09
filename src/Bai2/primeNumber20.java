74e7d448ec7d8542933ce2dfa211cc433e06e822
Biibdarkness <nguyenvantuananh8496@gmail.com>
2021-12-09 14:23:31 +0700

    20primeNumber

---
+++ src/Bai2/primeNumber20.java
@@ -0,0 +1,28 @@
+package Bai2;
+
+import java.util.Scanner;
+
+public class primeNumber20 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter Number: ");
+        int number = scanner.nextInt();
+        int count = 0;
+        int n = 2;
+        boolean isPrime = true;
+        while (count <= number) {
+            isPrime = true;
+            for (int i = 2; i <= Math.sqrt(n); i++) {
+                if (n%i==0){
+                    isPrime = false;
+                    break;
+                }
+            }
+            if (isPrime){
+                System.out.printf("%d \n", n);
+                count++;
+            }
+            n++;
+        }
+    }
+}
