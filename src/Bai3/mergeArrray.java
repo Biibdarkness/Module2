fb3725c15d53617eb92a2e3c44af9b70e22ad862
Biibdarkness <nguyenvantuananh8496@gmail.com>
2021-12-13 16:10:26 +0700

    findmax

--- src/Bai3/mergeArrray.java
+++ src/Bai3/mergeArrray.java
@@ -4,22 +4,23 @@ import java.util.Arrays;
 
 public class mergeArrray {
     public static void main(String[] args) {
-        int[] array1 = {1,2,3,4,5}; //Mảng khởi tạo 1
-        int[] array2 = {6,7,8,9,10}; //Mảng khởi tạo 2
-        int length = array1.length + array2.length; //Thêm độ dài của Mảng 1 vào Mảng 2
-        int[] array3 = new int[length]; //Mảng kết quả
+        int[] arr1 = {1,2,3,4,5}; //Mảng khởi tạo 1
+        int[] arr2 = {6,7,8,9,10}; //Mảng khởi tạo 2
+        int length = arr1.length + arr2.length; //Thêm độ dài của Mảng 1 vào Mảng 2
+        int[] arr3 = new int[length]; //Mảng kết quả
         int i = 0;
-        for (int element : array1) //Sao chép các phần tử của Mảng 2 bằng vòng lặp for-each
+        for (int element : arr1) //Sao chép các phần tử của Mảng 2 bằng vòng lặp for-each
         {
-            array3[i] = element;
+            arr3[i] = element;
             i++; //Tăng vị trí lên 1
         }
-        for (int element : array2) //sao chép các phần tử của Mảng 1 bằng vòng lặp for-each
+//        System.out.println(arr3[]);
+        for (int element : arr2) //sao chép các phần tử của Mảng 1 bằng vòng lặp for-each
         {
-            array3[i] = element;
+            arr3[i] = element;
             i++;
         }
-        System.out.println(Arrays.toString(array3)); //In mảng kết quả
+        System.out.println(Arrays.toString(arr3)); //In mảng kết quả
     }
 }
 
