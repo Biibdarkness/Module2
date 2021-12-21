cf6bc354f4be63479027d6f4a6f650f45df77814
Biibdarkness <nguyenvantuananh8496@gmail.com>
2021-12-21 14:09:25 +0700

    linkedlist

--- src/TuanAnh/Main.java
+++ src/TuanAnh/Main.java
@@ -1,10 +1,33 @@
 package TuanAnh;
 
+import java.util.*;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	Scanner scanner = new Scanner(System.in);
+//    Stack<String> stackChuoi = new Stack<String>();
+//    stackChuoi.push("value") => đưa giá trị vào stack
+//        stackChuoi.pop()  => lấy giá trị ra, xóa khỏi stack
+//        stackChuoi.peek() => lấy giá trị ra, không xóa khỏi stack
+//    stackChuoi.clear(); => xóa tất cả
+//    stackChuoi.contains("value") xác định giá trị có tồn tại trong stack hay không
+    Queue<String> danhsach = new PriorityQueue<String>();
 
-        System.out.println("Hello");
+//    danhsach.add() => chèn phần tử đã định vào hàng chờ
+//        danhsach.offer() => chèn vào
+//        danhsach.element() => trả về phần đầu của hàng đợi
+//    danhsach.peek() => trả về phần đầu của hàng đợi null
+//        danhsach.remove() => trả về và  xóa phần đầu của hàng đợi
+//        danhsach.poll() => trả về và loại bỏ phần đầu của hàng đợi null
+    danhsach.offer("tuan anh");
+        danhsach.offer("duong");
+        danhsach.offer("dan");
+        danhsach.offer("khoan");
+
+String ten = danhsach.poll();
+        System.out.println(ten);
     }
+
+
 }
---
+++ src/TuanAnh/demo.java
@@ -0,0 +1,4 @@
+package TuanAnh;
+
+public class demo {
+}
